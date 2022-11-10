e1913637aad5e3ece19b9cfce0be758809746a1f
Rosalie Wagner <mail@rosaliewagner.com>
2022-11-10 19:01:03 +0100

    Added build script with config.yaml for builder and python patch for style linking stuff

---
+++ sources/italic-patch.py
@@ -0,0 +1,15 @@
+import glob
+from fontTools.ttLib import TTFont
+from fontTools.ttLib.tables._n_a_m_e import NameRecord
+
+if __name__ == "__main__":
+
+    for font_path in glob.glob("../fonts/variable/*Italic*.ttf"):
+        with open(font_path, "rb") as f:
+            print("Fix italic style linking in {}".format(font_path))
+            ttFont = TTFont(f)
+            ttFont["post"].italicAngle = -12
+            ttFont["hhea"].caretSlopeRise = 1000
+            ttFont["hhea"].caretSlopeRun = 213
+            ttFont["name"].setName("MonaSansItalic", 25, 3, 1, 0x409)
+            ttFont.save(font_path)
