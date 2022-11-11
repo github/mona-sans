e1ba805ec64e864b9ff0d9b79a5cd55142c9643f
Rosalie Wagner <mail@rosaliewagner.com>
2022-11-11 13:34:54 +0100

    Added missing nameIDs

--- sources/italic-patch.py
+++ sources/italic-patch.py
@@ -12,4 +12,8 @@ if __name__ == "__main__":
             ttFont["hhea"].caretSlopeRise = 1000
             ttFont["hhea"].caretSlopeRun = 213
             ttFont["name"].setName("MonaSansItalic", 25, 3, 1, 0x409)
+            ttFont["name"].setName("ExtraLight", 261, 3, 1, 0x409)
+            ttFont["name"].setName("Regular", 263, 3, 1, 0x409)
+            ttFont["name"].setName("ExtraLight", 261, 1, 0, 0)
+            ttFont["name"].setName("Regular", 263, 1, 0, 0)
             ttFont.save(font_path)
